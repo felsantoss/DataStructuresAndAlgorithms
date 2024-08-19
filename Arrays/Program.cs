8fda12a18d309f39b58249842ab4c941d77470ea
Felipe Santos <felipesantossf@outlook.com>
2024-08-19 20:40:51 -0300

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿// Array com valores definidos
+
+string[] heroes = { "Spider Man", "Batman", "Iron Man" };
+
+for (int i = 0; i < heroes.Length; i++)
+{
+	var rank = i + 1;
+
+	Console.WriteLine($"{rank}. {heroes[i]}");
+}
+Console.Clear();
+// Array sem valor definido
+
+string[] animes = new string[4];
+
+animes[0] = "Attack On Titan";
+animes[1] = "Fullmetal";
+animes[2] = "Mob Psycho 100";
+animes[3] = "Death Note";
+
+for (int i = 0; i < animes.Length; i++)
+{
+	var rank = i + 1;
+
+	Console.WriteLine($"{rank}. {animes[i]}");
+}
