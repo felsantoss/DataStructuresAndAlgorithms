d0497a028c8fe142d3ca847fd3dc7b39d2dc1769
Felipe Santos <felipesantossf@outlook.com>
2024-08-19 20:57:50 -0300

    Removendo itens da lista

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -10,3 +10,13 @@ foreach (string item in shoppingList)
 {
 	Console.WriteLine(item);
 }
+
+shoppingList.Remove("Dreams");
+shoppingList.RemoveAt(1);
+
+Console.WriteLine("\nNew list after remove itens");
+
+for (int i = 0; i < shoppingList.Count; i++)
+{
+	Console.WriteLine(shoppingList[i]);
+}	
