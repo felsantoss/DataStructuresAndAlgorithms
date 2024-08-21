abd8e32f86a14cb4945996b95951409443b7cee8
Felipe Santos <felipesantossf@outlook.com>
2024-08-20 21:07:55 -0300

    Adicionando valores no jagged array

--- Jagged/Program.cs
+++ Jagged/Program.cs
@@ -2,4 +2,5 @@
 // Declare a jagged array.
 int[][] jaggedArray = new int[6][];
 
-// Set the values pf the first arraay in the jagged array structure.
+// Set the values of the first arraay in the jagged array structure.
+jaggedArray[0] = [1, 2, 3, 4];
