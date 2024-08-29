5a976704251f081ad6118d9b104139ae3e0bd4e5
Felipe Santos <felipesantossf@outlook.com>
2024-08-29 19:46:18 -0300

    Busca linear, interar sobre o array até encontrar o item alvo

---
+++ Searching/Program.cs
@@ -0,0 +1,23 @@
+﻿// Linear search algorithm
+
+string[] fruits = { "Orange", "Apple", "Banana" };
+string target = "Apple";
+int index = -1;
+
+for (int i = 0; i < fruits.Length; i++)
+{
+	if (fruits[i] == target)
+	{
+		index = i;
+		break;
+	}
+}
+
+if (index != 0)
+{
+	Console.WriteLine($"Element found at index: {index}");
+}
+else
+{
+	Console.WriteLine("Element not found.");
+}
