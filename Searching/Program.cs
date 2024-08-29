7e5796e7bc1491253043db77cb1fd0ef6d3db5a2
Felipe Santos <felipesantossf@outlook.com>
2024-08-29 19:53:10 -0300

    filtrando itens com mais de 5 caracteres

--- Searching/Program.cs
+++ Searching/Program.cs
@@ -21,3 +21,19 @@ else
 {
 	Console.WriteLine("Element not found.");
 }
+
+// Applying conditional statements and loops for filtering
+List<string> filteredFtruits = new List<string>();
+
+for (int i = 0; i < fruits.Length; i++)
+{
+	if (fruits[i].Length > 5)
+	{
+		filteredFtruits.Add(fruits[i]);
+	}
+}
+
+foreach (var fruit in filteredFtruits)
+{
+	Console.WriteLine(fruit);
+}
