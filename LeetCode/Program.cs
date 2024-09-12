7b5cfd5197be12121cda423da6d6f07716726660
Felipe Santos <felipesantossf@outlook.com>
2024-09-12 20:43:52 -0300

    Resolvendo leetcode 1346 - Check if N and Its Double Exists https://leetcode.com/submissions/detail/1388213425/

---
+++ LeetCode/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1346 - Check if N and Its Double Exists
+
+bool CheckIfExist(int[] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		for (int j = i + 1; j < arr.Length; j++)
+		{
+			if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i])
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
