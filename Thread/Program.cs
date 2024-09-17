9c1bd79df420d5623b05a9df1cfba8669cb69d23
Felipe Santos <felipesantossf@outlook.com>
2024-09-17 20:33:56 -0300

    Manipulação de Thread com C#

---
+++ Thread/Program.cs
@@ -0,0 +1,17 @@
+﻿Thread thread = new Thread(NewThread);
+thread.Start();
+
+for (int i = 0; i < 5; i++)
+{
+	Console.WriteLine("Main Thread: " + i);
+	Thread.Sleep(1000);
+}
+
+static void NewThread()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		Console.WriteLine("New Thread: " + i);
+		Thread.Sleep(1000);
+	}
+}
