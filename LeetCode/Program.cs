8545f3682e2f760ebd28f8fd72bce2f95bd2c4b4
Felipe Santos <felipesantossf@outlook.com>
2024-09-18 19:59:42 -0300

    Valid Montain Array - https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3251/

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -14,3 +14,26 @@ bool CheckIfExist(int[] arr)
 	}
 	return false;
 }
+
+// 941 - Valid Mountain Array
+
+bool ValidMountainArray(int[] arr)
+{
+	int i = 0;
+
+	while (i < arr.Length && i + 1 < arr.Length && arr[i] < arr[i + 1])
+		i++;
+	
+
+	if (i == 0 || i + 1 >= arr.Length)
+		return false;
+	
+
+	while (i < arr.Length && i + 1 < arr.Length)
+	{
+		if (arr[i] <= arr[i++ + 1])
+			return false;
+	}
+
+	return true;	
+}
